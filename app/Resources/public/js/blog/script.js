42d8ea084fb0d6afb00157575b0c41b44cb662e3
silga joseph <silga82@yahoo.fr>
2019-01-18 20:26:46 +0100

    installation et configuration boostrap 3 et assetic

---
+++ app/Resources/public/js/blog/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    console.log( "ready!mmmmmmm" );
+});
+
+/*$.fn.datepicker.defaults.language = 'fr';
+$.fn.datepicker.defaults.autoclose = true;
+$('#date').datepicker({
+    startDate: '-33m',
+    todayHighlight: true,
+    format:'dd/mm/yyyy' // format symfony correspondant : 'format' =>'dd/MM/yyyy'
+   
+});*/
